0353741bda8590cfc441bab1e535727e274b0bf1
digitalemekci <digitalemekci@gmail.com>
2022-07-27 18:40:13 +0300

    test

--- app/Mail/SendMail.php
+++ app/Mail/SendMail.php
@@ -18,21 +18,13 @@ class SendMail extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
+
     public function __construct($data)
     {
         $this->data = $data;
     }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
+
     public function build()
     {
         return $this->from('smtp@melinotravel.com')
